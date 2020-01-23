4f43401af486f2d7f6789d324794a244ab7f6ca8
Dan Foley <dan@dan-foley.com>
2020-01-23 19:25:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,5 +8,5 @@ def batch_badge_creator(name_array)
 end
 
 def assign_rooms(names)
-  names.collect {|name| "Hello, #{name}! You'll be assigned to room #{name.index + 1}!"}
+  names.each_with_index {|name| "Hello, #{name}! You'll be assigned to room #{name.index + 1}!"}
 end
